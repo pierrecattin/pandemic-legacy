20d29ac7433233667c5f1fa954599fa04eb4c520
Pierre Cattin <cattin.pierre@gmail.com>
2021-02-15 16:04:26 +0100

    comments

--- run_app.R
+++ run_app.R
@@ -6,12 +6,11 @@ deps <- c("openxlsx",
           "shiny", 
           "shinydashboard")
 lapply(X=deps, FUN=library, character.only=T)
-
 lapply(X=list.files("functions/",full.names = T), FUN = source)
 
 #### Data ####
 cards <- read.xlsx("data/all_cards.xlsx")
-# blocks <- init_blocks(cards) 
+
 #### App ####
 source("ui.R")
 source("server.R")
