9d8f6c3fc9bc781883fcdfb9bd98d1cda0fbc518
Pierre Cattin <cattin.pierre@gmail.com>
2022-02-14 20:01:58 +0100

    Enable return into game from any card, even if start outside of game

--- functions/init_blocks.R
+++ functions/init_blocks.R
@@ -1,15 +1,22 @@
 init_blocks <- 
   function(cards){
-    nb_cards <- sum(cards$count)
+    nb_cards <- 3*length(unique(cards$city))
     blocks <- 
       tibble(city=character(nb_cards), 
-             block=rep(0L,nb_cards))
+             block=integer(nb_cards))
     row <- 1
     for(i in 1:nrow(cards)){
-      if(cards$count[i]>0){
-        blocks$city[row:(row+cards$count[i]-1)] <- cards$city[i]
-        row <- row+cards$count[i]
+      blocks$city[row:(row+3-1)] <- cards$city[i]
+      
+      if(cards$count[i] >0 ){
+        ix <- row:(row+cards$count[i]-1)
+        blocks$block[ix] <- 0L
       }
+      if(cards$count[i] < 3 ){
+        ix <- (row+cards$count[i]):(row+3-1)
+        blocks$block[ix] <- -2L
+      }
+      row <- row+3
     }
     return(blocks)
   }
